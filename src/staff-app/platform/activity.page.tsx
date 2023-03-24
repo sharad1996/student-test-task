af58efa3a9f3e75242e087b3d0dcc773333d2d13
sharad1996 <sharadwankhade23@gmail.com>
2023-03-24 19:23:31 +0530

    Minor changes

--- src/staff-app/platform/activity.page.tsx
+++ src/staff-app/platform/activity.page.tsx
@@ -7,33 +7,59 @@ import { Student } from "utils/types";
 import { StudentListTile } from "staff-app/components/student-list-tile/student-list-tile.component";
 import { RollStateList } from "staff-app/components/roll-state/roll-state-list.component";
 import { filterStudentBasedOnState, getStudentList } from "utils";
+import { useApi } from "shared/hooks/use-api";
+import { Person } from "shared/models/person";
 
 export const ActivityPage: React.FC = () => {
+  const [getStudents, data, loadState] = useApi<{ students: Person[] }>({ url: "get-activities" })
   const { students  } = useContext(AppContext) as IApp;
   const [allStudents, setAllStudents] = useState<any>([]);
+  
+  useEffect(() => {
+    void getStudents()
+  }, [getStudents])
+
   useEffect(() => {
     setAllStudents(students);
   }, [])
 
+  const makeData = () => {
+
+  }
+  const getAllData = () => {
+    const formatedData: any = [];
+    data?.activity.map((act: any) => {
+      const obj: any = {};
+      obj.id = act.entity.id;
+      obj.first_name = act.entity.name.split(" ")[0];
+      obj.last_name = act.entity.name.split(" ")[1];
+      obj.state = act.entity.student_roll_states[0].roll_state;
+      formatedData.push(obj);
+    });
+    return formatedData;
+  }
+  useEffect(() => {
+    if (data?.activity) {
+      setAllStudents(getAllData());
+    }
+  }, [data])
   const onItemClick = (type: string) => {
     if (type === "all") {
-      setAllStudents(students);
+      setAllStudents(getAllData());
     } else {
-      const filteredStudents = filterStudentBasedOnState(students, type);
-      
+      const filteredStudents = filterStudentBasedOnState(getAllData(), type);
       setAllStudents(filteredStudents);
     }
   }
-
   return (
     <S.Container>
       <S.TopHeader>
         <RollStateList
             stateList={[
-              { type: "all", count: getStudentList("all", students)},
-              { type: "present", count: getStudentList("present", students)},
-              { type: "late", count: getStudentList("late", students)},
-              { type: "absent", count: getStudentList("absent", students) },
+              { type: "all", count: data?.activity?.length},
+              { type: "present", count: getStudentList("present", getAllData())},
+              { type: "late", count: getStudentList("late", getAllData())},
+              { type: "absent", count: getStudentList("absent", getAllData()) },
             ]}
             onItemClick={onItemClick}
           />
